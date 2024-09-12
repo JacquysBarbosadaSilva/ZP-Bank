295083aeaf65d850dadab520b4fe7ff56a12a56b
JacquysBarbosadaSilva <jacquys0801@gmail.com>
2024-09-12 15:02:19 -0300

    Alterações em todas as páginas

--- js/main.js
+++ js/main.js
@@ -1,37 +1,16 @@
-function novaTransacao() {
-    const addScreen = document.getElementById('add-screen');
-    addScreen.style.display = 'block';
-  }
-  
-  // Adicione um evento de clique ao botão de fechar (opcional)
-  document.getElementById('add-submit').addEventListener('click', () => {
-    const addScreen = document.getElementById('add-screen');
-    addScreen.style.display = 'none';
-  });
+function alternarConteudo(button) {
+  const trocarConteudo = button.nextElementSibling;
+  trocarConteudo.classList.toggle("show");
+}
 
-  function novaTransacao() {
-    const addScreen = document.getElementById('add-screen');
-    addScreen.classList.add('show');
-  }
+// Simulador
 
-  // Educação
 
-  document.addEventListener("DOMContentLoaded", function() {
-    var coll = document.getElementsByClassName("botao-juros");
-    for (var i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function() {
-            var content = this.nextElementSibling.querySelector(".escrita-card").innerHTML;
-            this.innerHTML = content;
-            for (var j = 0; j < coll.length; j++) {
-                if (coll[j] !== this) {
-                    coll[j].classList.remove("active");
-                    coll[j].innerHTML = coll[j].getAttribute("data-original");
-                }
-            }
-            this.classList.toggle("active");
-        });
-    }
-    for (var i = 0; i < coll.length; i++) {
-        coll[i].setAttribute("data-original", coll[i].innerHTML);
-    }
-});
+// Linha-Meses
+$("input[type=range]").on("change input", function() {
+  $("[name=values]").val($(this).val())
+})
+
+//   Resultado da Simulação
+
+var total = document.getElementById('valor','meses', 'taxa-juros');
